b6d133bc26247d2e2d9c1bd3e92e9e6fc73ba327
JefferyL1 <jefferyl@mit.edu>
2024-11-11 23:51:54 -0500

    Update job_submission.sh

--- training_and_analysis/job_submission.sh
+++ training_and_analysis/job_submission.sh
@@ -1,1 +1,21 @@
+#!/bin/bash
+#SBATCH -J LEMBAS_w_attn_VCAP
+#SBATCH -o LEMBAS_w_attn_VCAP%j.out
+#SBATCH -e LEMBAS_w_attn_VCAP%j.err
+#SBATCH --mail-user=jefferyl@mit.edu
+#SBATCH --mail-type=ALL
+#SBATCH --gres=gpu:4
+#SBATCH --gpus-per-node=4
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=8
+#SBATCH --mem=0
+#SBATCH --time=12:00:00
+#SBATCH --exclusive
+
+. /nobackup/users/jefferyl/miniforge3/etc/profile.d/conda.sh
+
+conda activate ml_env
+
+python 
+
 
