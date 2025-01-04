636e6af6121a83f17e4005087e52102e30c10a71
JefferyL1 <jefferyl@mit.edu>
2025-01-03 21:53:44 -0500

    Update utilities.py

--- training_and_analysis/off_target_training/utilities.py
+++ training_and_analysis/off_target_training/utilities.py
@@ -35,6 +35,22 @@ def set_cores(n_cores: int):
     os.environ["VECLIB_MAXIMUM_THREADS"] = str(n_cores)
     os.environ["NUMEXPR_NUM_THREADS"] = str(n_cores)
 
+def get_lr_cos(iter:int, max_iter: int = 1000, max_height: float = 1e-4, end_height: float = 1e-5):
+    """ Calculates learning rate for a given iteration during training using cosine scheduler.
+
+    Parameters
+    __________
+    iter: current iteration
+    max_iter: maximum number of training iterations
+    max_height: maximum learning rate parameter
+    start_height: initial learning rate
+    end_height: final learning rate
+
+    Returns: learning rate (float)
+    """
+    cos_value = 1 + math.cos((math.pi*iter)/max_iter)
+    return (max_height - end_height)/2 * cos_value + end_height
+    
 def get_lr_new(iter:int, max_iter: int = 1000, max_height: float = 1e-4,
            start_height: float = 1e-5, end_height: float = 1e-5,
            peak: int = 200):
